984aef99e62afe82887fe24f68e7178f45571ace
leov <leo.verbeke@hotmail.fr>
2022-06-06 21:28:32 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+        consectetur, officia veniam saepe numquam neque non magni autem at
+        suscipit! Quidem necessitatibus maxime a, iusto dicta officia ullam
+        reiciendis debitis harum, veniam enim velit! Repudiandae eum labore
+        ducimus magni provident quae ut, inventore fugiat eveniet sapiente
+        molestiae. Nobis harum corporis facilis eveniet cupiditate dolorum quas
+        ipsam exercitationem veritatis molestias illo dolore distinctio optio
+        quaerat, iusto error. Aut impedit nemo repellendus voluptates dolor
+        ratione minima vitae unde ipsa totam repellat, sunt cupiditate
+        voluptatibus quasi animi tenetur obcaecati voluptate quia doloremque eum
+        consequuntur. Placeat ipsum consequuntur ipsa quo minus quisquam modi
+        hic.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione soluta
+        voluptatum molestiae, aperiam magni obcaecati quia tempora, architecto
+        quaerat fugiat modi officia animi, doloribus aspernatur perferendis
+        illum quod blanditiis deserunt odio laboriosam cum laborum? Facere,
+        incidunt! Iste, quae. Architecto minus odio repellendus, eum quas ab at
+        aliquam nemo quos molestias magni possimus vitae sint illo inventore
+        natus incidunt fugiat, recusandae cum perferendis tempore ipsam quam
+        impedit itaque? Obcaecati sint quas cupiditate libero, esse, architecto
+        ea rem doloribus sit vero cum nobis expedita voluptates. Minima
+        distinctio voluptas assumenda itaque maxime quasi libero soluta dolorum
+        quos enim vel magnam eos, deserunt facere?
+      </p>
+    </div>
+  );
+};
+
+export default About;
